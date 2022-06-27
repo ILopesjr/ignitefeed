7682b037308a5256165391eb1d099fcf248f543b
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2022-06-27 10:03:21 -0300

    Criando o componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styles from './Sidebar.module.css';
 
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/ILopesjr.png" />
+        <Avatar src="https://github.com/ILopesjr.png" />
         <strong>Ivanildo Lopes</strong>
         <span>Web Developer</span>
       </div>
